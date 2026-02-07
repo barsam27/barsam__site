1b242a04030f554fda614f840df5c5f0bf7221ce
barsam27 <arshamshabestani@gmail.com>
2026-02-07 19:40:12 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function toggleMenu() {
 }
 </script>
 
+
+const toggleBtn = document.getElementById("darkToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
+
+if (localStorage.getItem("mode") === "dark") {
+    document.body.classList.add("dark");
+}
