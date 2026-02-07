d9729abfb970e514c693c0bad7bc0d6724f1d609
barsam27 <arshamshabestani@gmail.com>
2026-02-07 20:05:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,3 @@ function toggleMenu() {
 </script>
 
 
-const toggleBtn = document.getElementById("lightToggle");
-
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light");
-
-    if (document.body.classList.contains("light")) {
-        localStorage.setItem("mode", "light");
-    } else {
-        localStorage.setItem("mode", "normal");
-    }
-});
-
-// لود شدن حالت قبلی
-if (localStorage.getItem("mode") === "light") {
-    document.body.classList.add("light");
-}
