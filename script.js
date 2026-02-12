a5d9542e475804d71253b44b299bd5173d031d20
barsam27 <arshamshabestani@gmail.com>
2026-02-12 15:02:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ function toggleMenu() {
 }
 </script>
 
-
-<script>
-let cartCount = 0;
-const cart = document.getElementById('cart');
-const cartCounter = document.getElementById('cart-count');
-
-cart.addEventListener('click', ()=>{
-  window.location.href = 'checkout.html';
-});
-
-function addToCart() {
-  cartCount++;
-  cartCounter.textContent = cartCount;
-}
-</script>
